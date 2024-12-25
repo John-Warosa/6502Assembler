ffe347240562a04271b420be508f8e9af05681a0
John-Warosa <elias.mat100@gmail.com>
2024-12-25 11:44:58 +0100

    restart

--- test/test.c
+++
@@ -1,9 +0,0 @@
-#include "test_readfile.h"
-
-
-int main() {
-	// test readfile
-	test_is_asm_file();
-
-	return 0;
-}
--- test/test_readfile.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEST_READFILE_H
-#define TEST_READFILE_H
-
-
-void test_is_asm_file();
-
-
-#endif //TEST_READFILE_H
